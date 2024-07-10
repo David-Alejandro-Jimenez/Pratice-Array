00ba2cd4ec54a7f4280244540def7957aeed6e4f
Alejandro Jiménez <dlejandro15j@gmail.com>
2024-07-10 12:43:45 -0500

    Modificacion.2

--- main_test.go
+++ main_test.go
@@ -1,16 +1,23 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestArrays(t *testing.T) {
-	numbers1 := []int{1,2,3,4,5}
-	numbers2 := []int{0,9,8,7,0}
+	var test = []struct {
+		numbers1 []int
+		numbers2 []int
+		result []int
+	}{
+		{[]int{1,2,3,4,5}, []int{0,9,8,7,0}, []int{1,0,2,9,3,8,4,7,5,0}},
+	}
 
-	result := ArraysPrueba(numbers1, numbers2)
-	for i := range numbers1 {
-		result = append(result, numbers1[i], numbers2[i])
-		if result == nil {
-			t.Errorf("Practica incorrecta")
+	for _, i := range test {
+		var res = slices.Equal(ArraysPrueba(i.numbers1, i.numbers2), i.result)
+		if res != true {
+			t.Errorf("Practica incorrecta esperabamos una union entre %v y %v pero obtuvimos otra union inesperada %v", i.numbers1, i.numbers2, i.result)
 		}
-	} 
-} 
+	}
+}
