b72ace524720f057063f2c4e47141c0dd3508b4e
Alejandro Jiménez <dlejandro15j@gmail.com>
2024-07-18 19:48:02 -0500

    Modificacion.3

---
+++ Readme.md
@@ -0,0 +1,10 @@
+### Solucion a prueba tecnica
+
+- Esta solucion a una prueba sencilla se realizo con go.
+- Su principal funcion es alternar los valores de dos arrays.
+- No pude resolver esta prueba en el momento en el me lo pidieron(Pero a la final lo pude realizar).
+- Dejo la solucion adjunta.
+- Tiene pruebas unitarias que han comprobado que el codigo funciona correctamente.
+- Si alguien tiene alguna sugerencia o consejo me lo pueden comentar.
+
+# Fin
--- main.go
+++ main.go
@@ -4,20 +4,18 @@ import (
 	"fmt"
 )
 
-//Creamos en una funcion los arrays y su tipo de dato
+//La funcion ArraysPrueba se encarga de alternar valores de dos arrays y poner el resultado en un array vacio
 func ArraysPrueba(array1, array2 []int) (resultado []int) {
-	//Creamos un ciclo for para que alterne los 2 arrays
 	for i := range array1 {
 		resultado = append(resultado, array1[i], array2[i])
 	}
-	//Imprimimos El Resultado.
+
 	fmt.Println(resultado[0:9])
-	//retornamos el resultado del ciclo for
+
 	return resultado 
 	
 }
 
 func main() {
-	//Por ultimo llamamos a la fucnion ArraysPrueba y le asignamos los valores al Array
 	ArraysPrueba([]int{1,2,3,4,5}, []int{0,9,8,7,0})
 }
